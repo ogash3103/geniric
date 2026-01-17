cef7ff96cb8cc05f04247bf7a4dadcb38465774d
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2026-01-17 18:31:33 +0500

    task 2 done

--- bin/geniric.dart
+++ bin/geniric.dart
@@ -1,9 +1,24 @@
+// task 1
 T add<T extends num>(T num1, T num2) => (num1 + num2) as T;
 
+// task 2
+
+T? firstElementReturn<T>(List<T> list) {
+  if(list.isEmpty) return null;
+    return list.first;
+}
+
 void main() {
+  // task 1
   double n1 = 12.7;
   int n2 = 13;
   var result = add(n1, n2);
-
   print(result);
+
+  // task 2
+
+  List list = [12, "String", 14.3];
+  var result2 = firstElementReturn(list);
+  print(result2);
+
 }
